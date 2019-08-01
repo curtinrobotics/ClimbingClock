35984fbe25017a9bb6d3684e80f2004834cb7bd1
Unton4ik <unton4ik@gmail.com>
2019-08-01 18:53:19 +0800

    second RTC commit
    added the previous12Hour function

--- Tortoise_Code/Tortoise_Code.ino
+++ Tortoise_Code/Tortoise_Code.ino
@@ -1,7 +1,7 @@
 /*
- * Author: Anton R
+ * Authors: Harrison O, Anton R
  * Date: 01/08/2019
- * Purpose: Added Real Time Clock functionality
+ * Purpose: Code for the tortoise component of the clock
  * Project: Climbing Clock (2019)
  * Organisation: Curtin Robotics Club (CRoC)
  */
@@ -11,7 +11,7 @@
 #include <RTClib.h> // Real Time Clock library
 
 const TimeSpan CORRECT_TIME = new TimeSpan(12*60*60); // Num of seconds in 12 hours
-const uint16_t INITIAL_PWM = 300; //dummy value for bug testing. Get real value via testing robot.
+const uint16_t INITIAL_PWM = 300; // dummy value for bug testing. Get real value via testing robot.
 
 uint16_t currentPwm, correctedPwm;
 bool topMet = false;
@@ -26,7 +26,7 @@ void setup() {
   rtc.begin(); // start the real time clock
 
   DateTime startingTime = rtc.now(); // time at startup
-  DateTime initalCorrectTime = new DateTime(CORRECT_TIME.unixtime() - startingTime.unixtime());
+  TimeSpan initalCorrectTime = new DateTime(previous12Hour(startingTime) - startingTime);
 
   do {
     topMet = checkIfAtTop();
@@ -52,7 +52,7 @@ bool checkIfAtTop(void) {
   return true; //used for testing purposes
 }
 
-// returns current time
+// returns time
 DateTime getTime(void) {
   return rtc.now();
 }
@@ -65,6 +65,21 @@ void prepareNextCycle() {
   topMet = false;
   prevTime = getTime();
   delay(300); //prevents topMet from being reactivated
+}
+
+DateTime previous12Hour(DateTime time) {
+  DateTime out = new Datetime(time.unixtime());
+
+  if (time.hour() < 12) {
+    out -= time.hour();
+  }
+  else if (time.hour() > 12) {
+    out -= time.hour() - 12;
+  }
+
+  out -= time.minute() * 60 + time.second();
+
+  return out;
 }
 
 //go back to the bottom of the rack/ladder
