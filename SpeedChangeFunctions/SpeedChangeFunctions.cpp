71e5dd37372e8b23be6271029035fcf505e75c76
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-09-05 19:30:24 +0800

    Update SpeedChangeFunctions.cpp

--- SpeedChangeFunctions/SpeedChangeFunctions.cpp
+++ SpeedChangeFunctions/SpeedChangeFunctions.cpp
@@ -1,7 +1,7 @@
 /*
  * Author: Harrison Outram
  * Date: 5/09/2019
- * Version: 1.0
+ * Version: 1.1
  * Purpose: Simple functions for changing speedCorrector
  * Project: Climbing Clock (2019)
  * Organisation: Curtin Robotics Club (CRoC)
@@ -11,18 +11,18 @@
 #include "SpeedChangeFunctions.h"
 
 //does not change speed increment
-static uint32_t SpeedChangeFunctions::noChange(uint32_t currSpeedInc, uint32_t speedIncChange) {
+static uint8_t SpeedChangeFunctions::noChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc;
 }
 
 //changes speed increment linearly
 // speedInc = initialSpeedInc - floor(time / correctTime) * speedIncChange
-static uint32_t SpeedChangeFunctions::linearChange(uint32_t currSpeedInc, uint32_t speedIncChange) {
+static uint8_t SpeedChangeFunctions::linearChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc -= speedIncChange;
 }
 
 //changes speed increment exponentially
 // speedInc = initialSpeedInc * (1 / speedIncChange) ^ floor(time / correctTime)
-static uint32_t SpeedChangeFunctions::exponentialChange(uint32_t currSpeedInc, uint32_t speedIncChange) {
+static uint8_t SpeedChangeFunctions::exponentialChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc /= speedIncChange;
 }
