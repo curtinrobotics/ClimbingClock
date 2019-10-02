bfd909a65711bbad5b0f68e1e1c27b25ada16381
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-10-02 11:25:03 +0800

    Added comments and improved readability

--- Code_Samples/Stepper_Driver_OrgTomVersion.ino
+++ Code_Samples/Stepper_Driver_OrgTomVersion.ino
@@ -1,9 +1,16 @@
-#define DIR 2
+/*
+  Author: Some bloke named "blueicesparks" ??????
+  Date: ????
+  Organisation: Curtin Robotics Club (CRoC)
+  Project: Climbing Clock (2019)
+  Purpose: Stepper motor controller thingy ????
+*/
+
+#define DIR 2 // Direction? directory? Dirac? What is this???
 #define STEP 3
-#define _EN 4
+#define _EN 4 // Encoder? Envelope? Enormous Nob? Again, what is this???
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(2, OUTPUT); digitalWrite(2, HIGH);
   pinMode(3, OUTPUT); digitalWrite(2, HIGH);
   pinMode(8, OUTPUT); digitalWrite(8, LOW);
@@ -12,12 +19,15 @@ void setup() {
   pinMode(4, OUTPUT); digitalWrite(4, LOW);
 }
 
-float f = 0;
+float f = 0.0; // You have very weird naming standards blueicesparks
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(DIR, sin(f)>0 ? HIGH : LOW);
-  tone(STEP, 1000*fabs(sin(f)));
-  f +=0.001;
-  if(f > 10) { f = 0; digitalWrite(_EN, digitalRead(_EN) ? LOW : HIGH ); }
+  digitalWrite(DIR, sin(f) > 0 ? HIGH : LOW);
+  tone(STEP, 1000 * fabs(sin(f)));
+  f += 0.001;
+  
+  if (f > 10.0) {
+    f = 0.0;
+    digitalWrite(_EN, digitalRead(_EN) ? LOW : HIGH );
+  }
 }
