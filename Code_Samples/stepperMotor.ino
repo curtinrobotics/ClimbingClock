ec1633cce29cd0dabacb2266ea9b1ec407038df2
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-10-02 11:44:59 +0800

    Added Comments for Review
    Several lines of code are difficult to understand.
    Relevant lines have had comments added asking for clarity.

--- Code_Samples/stepperMotor.ino
+++ Code_Samples/stepperMotor.ino
@@ -1,24 +1,36 @@
+/*
+  Author: Some bloke calling itself "blueicesparks" ?????
+  Date: ???????
+  Organisation: Curtin Robotics Club (CRoC)
+  Project: Climbing Clock (2019)
+  Purpose: something to do with stepper motors apparently ????
+*/
+
 #include <Tone.h>
 
-#define HDIR 2
-#define HSTP 3
-#define H_EN 4
+// What do this words mean???
+#define HDIR 2 // High as dirt?
+#define HSTP 3 // H-stop? Hate Sleeping Tiki-Parrots???
+#define H_EN 4 // Hate Every Nagger???
 
-#define MDIR 5
-#define MSTP 6
-#define M_EN 7
+#define MDIR 5 // Make Dodgeball Illegal in Romania???
+#define MSTP 6 // Men Stopping Tchikita Police???
+#define M_EN 7 // MASSIVES: Everyone's Nuisance???
 
-Tone hsm;
-Tone msm;
+Tone hsm; // Herpes Strike Miami ???
+Tone msm; // Men Seeing Men ???
 
 void setup()
 {
-  DDRD |= 0xFC;  // 0b11111100
+  // Is it necessary to specify the binary of a hexadecimal number?
+  // If someone reading this doesn't understand hexadecimal, how likely is it they'll understand binary?
+  DDRD |= 0xFC;  // 0b11111100 
   PORTD |= 0xFC; // 0b11111100
   hsm.begin(HSTP);
   msm.begin(MSTP);
 }
 
+// EEWWWWW!!! NOT ZOMBIE CODE!!!
 //tested speeds (*.play(speed*fabs(sin(f)))
 //original = 440
 //300 - 
@@ -30,21 +42,22 @@ void setup()
 //0.0005 - good but still too fast for the clock
 //0.0001 - doesn't seem to work?
 
-float f = 0; 
+float f = 0.0;
+
 void loop()
 {
-  digitalWrite(HDIR, sin(f)>0 ? HIGH : LOW);
-  digitalWrite(MDIR, sin(f)>0 ? HIGH : LOW);
+  digitalWrite(HDIR, sin(f) > 0 ? HIGH : LOW);
+  digitalWrite(MDIR, sin(f) > 0 ? HIGH : LOW);
   //hsm.stop();
-  hsm.play(300*fabs(sin(f))); //speed of motor 
+  hsm.play( 300 * fabs(sin(f)) ); //speed of motor 
   //msm.stop();
-  msm.play(300*fabs(sin(f)));  //speed of motor
+  msm.play( 300 * fabs(sin(f)) );  //speed of motor
   
-  f+=0.0005; //this will affect how long the gears spin for (it's a counter for the loop below)
+  f += 0.0005; //this will affect how long the gears spin for (it's a counter for the loop below)
 
-  if(f > 10) { 
-    f = 0;
-    PORTD ^= 0x90; 
+  if (f > 10) { 
+    f = 0.0;
+    PORTD ^= 0x90; // WHAT IN GOD'S NAME IS THIS???? ALWAYS comment on bitwise operations!!! 
     //digitalWrite(H_EN, digitalRead(H_EN) ? LOW : HIGH );  
     //digitalWrite(M_EN, digitalRead(M_EN) ? LOW : HIGH ); 
   }
