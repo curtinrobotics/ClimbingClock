fa923679d62178f1cb689ad2fb1ff6e2782bcf96
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-10-04 16:53:49 +0800

    Minor Readability Upgrades
    Replaced hexadecimal literals involved in bitwise operations with binary literals (easier to understand which pins change).
    Fixed mixed mode expressions.
    Removed unnecessary comments.

--- Code_Samples/stepperMotor.ino
+++ Code_Samples/stepperMotor.ino
@@ -1,7 +1,8 @@
 /*
   Author: Tom Johnson
   Date: 26/09/2019
-  Date: 28/09/2019 Modifications to speed and freq
+  Editor: Harrison Outram, Ashley 
+  Lase Updated: 4/10/2019 Modifications to speed and freq
   Organisation: Curtin Robotics Club (CRoC)
   Project: Climbing Clock (2019)
   Purpose: Revised version of the stepper motor code.
@@ -24,8 +25,8 @@ void setup()
 { 
   //DDRD - maps to the digital pins 0 - 7 on the Arduino board
   //PORTD - the read and write pin
-  DDRD |= 0xFC; //setting pins 2 - 7 as output, leaving pins 0 and 1 as inputs/nothing
-  PORTD |= 0xFC; //setting pins 2 -7 as output
+  DDRD |= 0b11111100; //setting pins 2 - 7 as output
+  PORTD |= 0b11111100; //setting pins 2 -7 as output
   motor1.begin(M1_STP); //initialising pin to play a tone
   motor2.begin(M2_STP); //initialising pin to play a tone
 }
@@ -34,19 +35,18 @@ float duration = 0.0;
 
 void loop()
 {
-  // if sin function > 0, then high will be written to pin. If sin function < 0, then low will be written to pin.
-  digitalWrite(M1_DIR, sin(duration) > 0 ? HIGH : LOW); 
+  digitalWrite(M1_DIR, sin(duration) > 0 ? HIGH : LOW);
   digitalWrite(M2_DIR, sin(duration) > 0 ? HIGH : LOW);
   //hsm.stop(); //stop playing the tone
-  motor1.play( 300 * fabs(sin(duration)) ); //speed of motor 
+  motor1.play( 300.0 * fabs(sin(duration)) ); //speed of motor 
   //msm.stop(); //stop playing the tone
-  motor2.play( 300 * fabs(sin(duration)) );  //speed of motor
+  motor2.play( 300.0 * fabs(sin(duration)) );  //speed of motor
   
   duration += 0.0005; //this will affect how long the gears spin for (it's a counter for the loop below)
 
-  if (duration > 10) { 
+  if (duration > 10.0) {
     duration = 0.0;
-    PORTD ^= 0x90; // resets the read/write pin (I'm pretty sure but will double check) 
+    PORTD ^= 0x10010000; // resets the read/write pin (I'm pretty sure but will double check) 
     //digitalWrite(H_EN, digitalRead(H_EN) ? LOW : HIGH );  
     //digitalWrite(M_EN, digitalRead(M_EN) ? LOW : HIGH ); 
   }
