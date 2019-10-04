b7c83388749414c635395701c473378b732a959d
blueicesparks <53563323+blueicesparks@users.noreply.github.com>
2019-10-04 10:19:25 +0800

    Update Stepper_Driver_OrgTomVersion.ino

--- Code_Samples/Stepper_Driver_OrgTomVersion.ino
+++ Code_Samples/Stepper_Driver_OrgTomVersion.ino
@@ -1,14 +1,14 @@
 /*
-  Author: Some bloke named "blueicesparks" ??????
-  Date: ????
+  Author: Tom LastName
+  Date: 26/09/2019
   Organisation: Curtin Robotics Club (CRoC)
   Project: Climbing Clock (2019)
-  Purpose: Stepper motor controller thingy ????
+  Purpose: To control the stepper motors for the Climbing Clock
 */
 
-#define DIR 2 // Direction? directory? Dirac? What is this???
-#define STEP 3
-#define _EN 4 // Encoder? Envelope? Enormous Nob? Again, what is this???
+#define DIR 2 // Direction
+#define STEP 3 // Step
+#define _EN 4 // Encoder
 
 void setup() {
   pinMode(2, OUTPUT); digitalWrite(2, HIGH);
@@ -19,15 +19,15 @@ void setup() {
   pinMode(4, OUTPUT); digitalWrite(4, LOW);
 }
 
-float f = 0.0; // You have very weird naming standards blueicesparks
+float freq = 0.0;
 
 void loop() {
   digitalWrite(DIR, sin(f) > 0 ? HIGH : LOW);
-  tone(STEP, 1000 * fabs(sin(f)));
-  f += 0.001;
+  tone(STEP, 1000 * fabs(sin(freq)));
+  freq += 0.001;
   
-  if (f > 10.0) {
-    f = 0.0;
+  if (freq > 10.0) {
+    freq = 0.0;
     digitalWrite(_EN, digitalRead(_EN) ? LOW : HIGH );
   }
 }
