3bd7ae7c72c6c1f4847c07099c4cba72c3df3ff4
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-10-04 16:57:09 +0800

    Minor Readability Upgrades
    Fixed mixed mode expressions.
    Moved global variable above the setup() function.

--- Code_Samples/Stepper_Driver_OrgTomVersion.ino
+++ Code_Samples/Stepper_Driver_OrgTomVersion.ino
@@ -1,6 +1,8 @@
 /*
   Author: Tom Johnson
   Date: 26/09/2019
+  Editor: Ashley, Harrison Outram
+  Last Updated: 4/10/2019
   Organisation: Curtin Robotics Club (CRoC)
   Project: Climbing Clock (2019)
   Purpose: To control the stepper motors for the Climbing Clock
@@ -9,6 +11,8 @@
 #define DIR 2 // Direction
 #define STEP 3 // Step
 #define _EN 4 // Encoder
+
+float duration = 0.0;
 
 void setup() {
   pinMode(2, OUTPUT); digitalWrite(2, HIGH);
@@ -19,11 +23,9 @@ void setup() {
   pinMode(4, OUTPUT); digitalWrite(4, LOW);
 }
 
-float duration = 0.0;
-
 void loop() {
-  digitalWrite(DIR, sin(duration) > 0 ? HIGH : LOW);
-  tone(STEP, 1000 * fabs(sin(duration)));
+  digitalWrite(DIR, sin(duration) > 0.0 ? HIGH : LOW);
+  tone( STEP, 1000.0 * fabs(sin(duration)) );
   duration += 0.001;
   
   if (duration > 10.0) {
