eda2e41a282e12c5d59f2552849c8ecd309ec5ec
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-11-28 19:40:35 +0800

    Code Review
    Added line of code reminding is to set new PWM.
    Removed memory leaks for DateTime objects in setup().

--- Tortoise_Code/Tortoise_Code.ino
+++ Tortoise_Code/Tortoise_Code.ino
@@ -1,6 +1,6 @@
 /*
  * Authors: Harrison O, Anton R
- * Date: 01/08/2019
+ * Date: 28/11/2019 (DD/MM/YYY)
  * Purpose: Code for the tortoise component of the clock
  * Project: Climbing Clock (2019)
  * Organisation: Curtin Robotics Club (CRoC)
@@ -13,7 +13,7 @@
 const TimeSpan CORRECT_TIME = new TimeSpan(12*60*60); // Num of seconds in 12 hours
 const uint16_t INITIAL_PWM = 300; // dummy value for bug testing. Get real value via testing robot.
 
-uint16_t currentPwm, correctedPwm;
+uint16_t currentPwm;
 bool topMet = false;
 DateTime prevTime;
 
@@ -22,21 +22,22 @@ SpeedCorrector speedCorrector(INITIAL_PWM, CORRECT_TIME);
 RTC_DS1307 rtc;
 
 void setup() {
-
   rtc.begin(); // start the rtc
-
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // initialize the rtc
 
   DateTime startingTime = rtc.now(); // time at startup
-  TimeSpan initalCorrectTime = new DateTime(previousTwelveHour(startingTime) - startingTime);
+  DateTime prevTwelveHour = getPreviousTwelveHour(startingTime);
+  TimeSpan *initialCorrectTime = new DateTime(prevTwelveHour - startingTime);
 
   do {
     topMet = checkIfAtTop();
 
-    if ( (topMet) || (getTime() == initalCorrectTime) ) {
+    if ( (topMet) || (getTime() == initialCorrectTime) ) {
       prepareNextCycle();
     }
   } while (!topMet);
+  
+  delete prevTwelveHour; delete initialCorrectTime;
 }
 
 void loop() {
@@ -61,20 +62,25 @@ DateTime getTime(void) {
 
 void prepareNextCycle() {
   resetPosition();
-  uint16_t tempPwm = speedCorrector.getCorrectedPwm(getTime(), currentPwm, topMet);
-  speedCorrector.addNewCorrectedPwm(tempPwm);
+  uint16_t correctedPwm = speedCorrector.getCorrectedPwm(getTime(), currentPwm, topMet);
+  speedCorrector.addNewCorrectedPwm(correctedPwm);
   currentPwm = speedCorrector.getMeanPwm();
   topMet = false;
+  //line setting new PWM pending
+  
+  // if reached top too early wait until time is up
+  while (getTime() - prevTime < CORRECT_TIME) { }
+  
   prevTime = getTime();
   delay(300); //prevents topMet from being reactivated
 }
 
 // returns the last noon or midnight, whichever is closest
-DateTime previousTwelveHour(DateTime time) {
-  DateTime out = new Datetime(time.unixtime());
+DateTime* getPreviousTwelveHour(DateTime time) {
+  DateTime *out = new Datetime(time.unixtime());
 
   if (time.hour() < 12) {
-    out -= time.hour();
+    *out -= time.hour();
   }
   else if (time.hour() > 12) {
     out -= time.hour() - 12;
