aa8bf783182ce9442deb3d98ee65b7cc1d33f1d1
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2019-12-08 14:43:50 +0800

    Minor updates
    Added better block comments, and replaced uint16_t with uint8_t since Arduino PWM uses uint8_t.

--- SpeedChangeFunctions/SpeedChangeFunctions.cpp
+++ SpeedChangeFunctions/SpeedChangeFunctions.cpp
@@ -1,7 +1,7 @@
 /*
  * Author: Harrison Outram
- * Date: 19/09/2019
- * Version: 1.1
+ * Date: 06/12/2019
+ * Version: 1.2
  * Purpose: Simple functions for changing speedCorrector
  * Project: Climbing Clock (2019)
  * Organisation: Curtin Robotics Club (CRoC)
@@ -10,19 +10,34 @@
 #include "Arduino.h"
 #include "SpeedChangeFunctions.h"
 
-//does not change speed increment
+/**
+ * does not change speed increment
+ * @param currSpeedInc The current speed increment
+ * @param speedIncChange The change of the speed increment (useless)
+ * @return uint8_t
+ */
 uint8_t SpeedChangeFunctions::noChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc;
 }
 
-//changes speed increment linearly
-// speedInc = initialSpeedInc - floor(time / correctTime) * speedIncChange
+/**
+ * Changes speed increment linearly
+ * speedInc = initialSpeedInc - floor(time / correctTime) * speedIncChange
+ * @param currSpeedInc The current speed increment
+ * @param speedIncChange The amount to decrease the speed increment by
+ * @return uint8_t
+ */
 uint8_t SpeedChangeFunctions::linearChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc - speedIncChange;
 }
 
-//changes speed increment exponentially
-// speedInc = initialSpeedInc * (1 / speedIncChange) ^ floor(time / correctTime)
+/**
+ * changes speed increment exponentially
+ * speedInc = initialSpeedInc * (1 / speedIncChange) ^ floor(time / correctTime)
+ * @param currSpeedInc The current speed increment
+ * @param speedIncChange What to divide the speed increment by
+ * @return uint8_t
+ */
 uint8_t SpeedChangeFunctions::exponentialChange(uint8_t currSpeedInc, uint8_t speedIncChange) {
 	return currSpeedInc / speedIncChange;
 }
