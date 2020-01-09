ec1cca164222ab61d9154c0b91f8eed616aca3d6
Harrison-O <53261102+Harrison-O@users.noreply.github.com>
2020-01-09 18:35:14 +0800

    Updated first comment block

--- SpeedCorrector/SpeedCorrector.cpp
+++ SpeedCorrector/SpeedCorrector.cpp
@@ -1,63 +1,63 @@
 /*
- * Author: Harrison Outram
- * Date: 12/12/2019 (d/m/y)
- * Version: 1.7
- * Purpose: Provide functionality for auto correcting motor speed
+ * @author Harrison Outram
+ * Last Updated: 13/12/2019 (d/m/y)
+ * @version 1.8
+ * @brief Provide functionality for auto correcting motor speed
  * Project: Climbing Clock (2019)
  * Organisation: Curtin Robotics Club (CRoC)
- * Working status: Tested and works
+ * Working status: works
  */
 
 #include "SpeedCorrector.h"
 
 /**
  * @param initialPwm The PWM the robot should start with
- * @param inCorrectTime The time it should take for the robot to complete each cycle
- * @param inSpeedChangeFunc The function which determines how the speedIncrement is changed
+ * @param correctTime The time it should take for the robot to complete each cycle in seconds
+ * @param speedChangeFunc The function which determines how the speedIncrement is changed
  */
-SpeedCorrector::SpeedCorrector(uint8_t initialPwm, uint32_t inCorrectTime,
-                    uint8_t (*inSpeedChangeFunc)(uint8_t currSpeedChange, uint8_t speedIncChange)) {
-    pwmIndex = 0;
-    maxPwmIndex = MAX_NUM_OF_PWMS - 1;
-    correctedPwms = new uint8_t[MAX_NUM_OF_PWMS];
-    correctedPwmsFull = false;
-    correctedPwms[0] = initialPwm;
-    correctTime = inCorrectTime;
-    speedIncrement = SPEED_INCREMENT;
-    minSpeedIncrement = MIN_SPEED_INCREMENT;
-    speedChangeFunc = inSpeedChangeFunc;
-    speedIncrementChange = SPEED_INCREMENT_CHANGE;
-    currPwm = initialPwm;
+SpeedCorrector::SpeedCorrector(uint8_t initialPwm, uint32_t correctTime,
+                                SpeedChangeFunc speedChangeFunc) {
+    _pwmIndex = 0;
+    _maxPwmIndex = MAX_NUM_OF_PWMS - 1;
+    _correctedPwms = new uint8_t[MAX_NUM_OF_PWMS];
+    _correctedPwmsFull = false;
+    _correctedPwms[0] = initialPwm;
+    _correctTime = correctTime;
+    _speedIncrement = SPEED_INCREMENT;
+    _minSpeedIncrement = MIN_SPEED_INCREMENT;
+    _speedChangeFunc = speedChangeFunc;
+    _speedIncrementChange = SPEED_INCREMENT_CHANGE;
+    _currPwm = initialPwm;
 }
 
 /**
  * @param initialPwm The PWM the robot should start with
- * @param inCorrectTime The time it should take for the robot to complete each cycle
- * @param inMaxNumOfCorrectedPwms The max num of corrected PWMs the object should store
- * @param inSpeedIncrement The amount the PWM should be increased by if the robot fails to reach the top in time
- * @param inMinSpeedIncrement The minimum amount the PWM should be increased by if the robot fails to reach the top in time
- * @param inSpeedChangeFunc The function which determines how the speedIncrement is changed
- * @param inSpeedIncrementChange Dictates how much the speedIncrement is changed by inSpeedChangeFunc
+ * @param correctTime The time it should take for the robot to complete each cycle in seconds
+ * @param maxNumOfCorrectedPwms The max num of corrected PWMs the object should store
+ * @param speedIncrement The amount the PWM should be increased by if the robot fails to reach the top in time
+ * @param minSpeedIncrement The minimum amount the PWM should be increased by if the robot fails to reach the top in time
+ * @param speedChangeFunc The function which determines how the speedIncrement is changed
+ * @param speedIncrementChange Dictates how much the speedIncrement is changed by inSpeedChangeFunc
  */
-SpeedCorrector::SpeedCorrector(uint8_t initialPwm, uint32_t inCorrectTime,    uint8_t inMaxNumOfPwms, 
-                    uint8_t inSpeedIncrement, uint8_t inMinSpeedIncrement,
-                    uint8_t (*inSpeedChangeFunc)(uint8_t currSpeedChange, uint8_t speedIncChange),
-                            uint8_t inSpeedIncrementChange) {
-    pwmIndex = 0;
-    maxPwmIndex = inMaxNumOfPwms - 1;
-    correctedPwms = new uint8_t[inMaxNumOfPwms];
-    correctedPwmsFull = false;
-    correctedPwms[0] = initialPwm;
-    correctTime = inCorrectTime;
-    speedIncrement = inSpeedIncrement;
-    minSpeedIncrement = inMinSpeedIncrement;
-    speedChangeFunc = inSpeedChangeFunc;
-    speedIncrementChange = inSpeedIncrementChange;
-    currPwm = initialPwm;
+SpeedCorrector::SpeedCorrector(uint8_t initialPwm, uint32_t correctTime,    uint8_t maxNumOfPwms, 
+                    uint8_t speedIncrement, uint8_t minSpeedIncrement,
+                    SpeedChangeFunc speedChangeFunc,
+                    uint8_t speedIncrementChange) {
+    _pwmIndex = 0;
+    _maxPwmIndex = maxNumOfPwms - 1;
+    _correctedPwms = new uint8_t[maxNumOfPwms];
+    _correctedPwmsFull = false;
+    _correctedPwms[0] = initialPwm;
+    _correctTime = correctTime;
+    _speedIncrement = speedIncrement;
+    _minSpeedIncrement = minSpeedIncrement;
+    _speedChangeFunc = speedChangeFunc;
+    _speedIncrementChange = speedIncrementChange;
+    _currPwm = initialPwm;
 }
 
 SpeedCorrector::~SpeedCorrector() {
-    delete correctedPwms;
+    delete _correctedPwms;
 }
 
     /* PUBLIC METHODS */
@@ -71,68 +71,90 @@ SpeedCorrector::~SpeedCorrector() {
 uint8_t SpeedCorrector::getCorrectedPwm(uint32_t actualTime, bool topReached) {
     uint8_t correctedPwm;
     
-    if (correctTime > actualTime)   // reached top too quickly
-        correctedPwm = currPwm - getPwmOffset(correctTime - actualTime);
-    else if (!topReached) {         // did not reach top in time
-        correctedPwm = currPwm + speedIncrement;
+    if (_correctTime > actualTime) {                 // reached top too quickly
+        correctedPwm = _currPwm - getPwmOffset(_correctTime - actualTime);
+    } else if (!topReached) {                       // did not reach top in time
+        if (_currPwm + _speedIncrement > MAX_PWM) {   // prevents overflow error
+            correctedPwm = MAX_PWM;
+        } else {
+            correctedPwm = _currPwm + _speedIncrement;
+        }
         calcNewSpeedIncrement();
-    } else {                        // speed just right
-        correctedPwm = currPwm;
+    } else {                                        // speed just right
+        correctedPwm = _currPwm;
     }
     
     return correctedPwm;
 }
 
 /**
- * Adds new corrected PWM to object
+ * Adds new corrected PWM to SpeedCorrector object
  * Also updates current PWM
  * @param correctedPwm The PWM required to complete the last cycle correctly
  * @return void
  */
 void SpeedCorrector::addNewCorrectedPwm(uint8_t correctedPwm) {
-    if (pwmIndex == maxPwmIndex) {
-        pwmIndex = 0; //reset index to replace oldest value
-        correctedPwmsFull = true;
+    if (_pwmIndex == _maxPwmIndex) {
+        _pwmIndex = 0; //reset index to replace oldest value
+        _correctedPwmsFull = true;
     } else {
-        pwmIndex++;
+        _pwmIndex++;
     }
-    correctedPwms[pwmIndex] = correctedPwm;
-    currPwm = getMeanPwm();
+    _correctedPwms[_pwmIndex] = correctedPwm;
+    _currPwm = getMeanPwm();
 }
 
+/**
+ * currPwm getter
+ * @return uint8_t
+ */
 uint8_t SpeedCorrector::getCurrentPwm(void) {
-    return currPwm;
+    return _currPwm;
 }
 
     /* PRIVATE METHODS */
 
+/**
+ * Generates mean PWM from corrected PWMs
+ * @return uint8_t
+ */
 uint8_t SpeedCorrector::getMeanPwm(void) {
     uint32_t meanPwm = 0;
     
-    if (correctedPwmsFull) {
-        for (uint8_t i = 0; i <= maxPwmIndex; i++)
-            meanPwm += correctedPwms[i];
+    if (_correctedPwmsFull) {
+        for (uint8_t i = 0; i <= _maxPwmIndex; i++)
+            meanPwm += _correctedPwms[i];
         
-        meanPwm /= (maxPwmIndex + 1);
+        meanPwm /= (_maxPwmIndex + 1);
     } else {
-        for (uint8_t i = 0; i <= pwmIndex; i++)
-            meanPwm += correctedPwms[i];
+        for (uint8_t i = 0; i <= _pwmIndex; i++)
+            meanPwm += _correctedPwms[i];
         
-        meanPwm /= (pwmIndex + 1);
+        meanPwm /= (_pwmIndex + 1);
     }
     
     return (uint8_t)meanPwm;
 }
 
+/**
+ * Calculates how much the PWM was off
+ * @param timeErr The difference between the actual time and the correct time
+ * @return uint8_t
+ */
 uint8_t SpeedCorrector::getPwmOffset(uint32_t timeErr) {
     //use dimensional analysis to confirm equation is correct
-    return (uint8_t)( ((float)timeErr / (float)correctTime) * (float)currPwm );
+    // Assumes PWM input and output scale linearly
+    return (uint8_t)( ((float)timeErr / (float)_correctTime) * (float)_currPwm );
 }
 
-//changes speedIncrement to new value, doesn't return it
+/**
+ * Changes speedIncrement to new value
+ * @return void
+ */
 void SpeedCorrector::calcNewSpeedIncrement(void) {
-    speedIncrement = (*speedChangeFunc)(speedIncrement, speedIncrementChange);
+    _speedIncrement = (*_speedChangeFunc)(_speedIncrement,
+                                         _speedIncrementChange);
 
-    if (speedIncrement < minSpeedIncrement)
-        speedIncrement = minSpeedIncrement;
+    if (_speedIncrement < _minSpeedIncrement)
+        _speedIncrement = _minSpeedIncrement;
 }
