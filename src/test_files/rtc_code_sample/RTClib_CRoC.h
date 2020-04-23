897e91fd66396c0ccbfdca25b2549c809a088db0
Unton4ik <unton4ik@gmail.com>
2020-04-23 19:25:11 +0800

    Added custom RTClib code

--- src/test_files/rtc_code_sample/RTClib_CRoC.h
+++ src/test_files/rtc_code_sample/RTClib_CRoC.h
@@ -3,42 +3,51 @@
    auth: anton r
 
    created:  06/04/2020
-   modified: 06/04/2020
-
-   purpose: TODO 
+   modified: 16/04/2020
 */
 
 #ifndef RTC_LIB_CROC_H
-#define RTC_LIB_CROC_H
+#  define RTC_LIB_CROC_H
 
-#include <Arduino.h>
-#include <stdint.h>
-#include <RTClib.h>
+#  include <RTClib.h>
 
-class RTC_DS1307_Wrapper;
-class DateTimeWrapper;
 
+/*
+   DateTimeWrapper:
+
+   Wrapper for the DateTime class
+*/
+class DateTimeWrapper : public DateTime
+{
+   public:
+      DateTimeWrapper(uint32_t t = SECONDS_FROM_1970_TO_2000)
+         : DateTime(t) {};
+      DateTimeWrapper(uint16_t year, uint8_t month, uint8_t day,
+         uint8_t hour = 0, uint8_t min = 0, uint8_t sec = 0)
+         : DateTime(year, month, day, hour, min, sec) {};
+      DateTimeWrapper(const DateTime &copy)
+         : DateTime(copy) {};
+      DateTimeWrapper(const __FlashStringHelper* date, const __FlashStringHelper* time)
+         : DateTime(date, time) {};
+      DateTimeWrapper(const char* date, const char* time)
+         : DateTime(date, time) {};
+
+      bool operator<(const DateTime &other);
+      bool operator>(const DateTime &other);
+      bool operator==(const DateTime &other);
+      bool operator<=(const DateTime &other);
+      bool operator>=(const DateTime &other);
+};
+
+/*
+   RTC_DS1307_Wrapper:
+
+   Wrapper for the RTC_DS1307 class, returns a DateTimeWrapper instead of DateTime
+*/
 class RTC_DS1307_Wrapper : public RTC_DS1307
 {
    public:
-      static uint32_t nowUnix(void);
-      static DateTimeWrapper nowDT(void);
-};
-
-class DateTimeWrapper : public DateTime
-{
-   private:
-      uint32_t time;
-   public:
-      DateTimeWrapper(uint32_t t = SECONDS_FROM_1970_TO_2000);
-      DateTimeWrapper(uint16_t year, uint8_t month, uint8_t day,
-            uint8_t hour = 0, uint8_t min = 0, uint8_t sec = 0);
-      DateTimeWrapper(const DateTime &copy);
-      //DateTimeWrapper(DateTimeWrapper &copy);
-      DateTimeWrapper(const char *date, const char *time);
-      DateTimeWrapper(const __FlashStringHelper *date, const __FlashStringHelper *time);
-
-      uint32_t unixtime(void);
+      DateTimeWrapper now() { return DateTimeWrapper(RTC_DS1307::now()); }
 };
 
 #endif
