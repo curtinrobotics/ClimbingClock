d340a7a0c130da2cdb4e876a905bac711647e780
Harrison-O <mr.outram995@gmail.com>
2020-04-30 19:47:15 +0800

    Created test scripts for interfaces, fixed typo, and improved readability.

--- src/main_files/TimeScale_code/ITimeScale/ITimeScale.h
+++ src/main_files/TimeScale_code/ITimeScale/ITimeScale.h
@@ -1,7 +1,7 @@
 /*
- * Author: Ashley Taylor
+ * @author Ashley Taylor
  * Date Created: 9/04/2020 (d/m/y, UTC+08:00)
- * @brief ??
+ * @brief Interface for Time Scale classes
  * @see README.md file for full breakdown. 
  */
 
@@ -12,7 +12,6 @@
 
 class ITimeScale
 {
-    
     public:
         virtual bool start() = 0;
         virtual void stop() = 0;
@@ -20,7 +19,6 @@ class ITimeScale
         virtual bool setTime(uint8_t hours, uint8_t minutes, uint8_t seconds) = 0;
 
         virtual ~ITimeScale() { Serial.println(F("ITimeScale destroyed")); }
-
 };
 
 #endif
